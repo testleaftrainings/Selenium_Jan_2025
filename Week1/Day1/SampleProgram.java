ebc9f7a6933cf2072fb293dc055683dfceea86ae
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-01-04 19:51:07 +0530

    Add files via upload

---
+++ Week1/Day1/SampleProgram.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class SampleProgram {
+	
+	
+	//type main--->do ctrl+space
+	public static void main(String[] args) {
+		//type syso--->ctrl+space
+		System.out.println("hello everyone");
+		System.out.println("happy learning");
+	
+	
+}
+}
