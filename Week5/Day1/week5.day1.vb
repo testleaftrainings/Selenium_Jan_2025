dcb411973a96d5bcaeb91b677dfdf2f81bc6823c
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-02-01 20:37:39 +0530

    Add files via upload

---
+++ Week5/Day1/week5.day1.vb
@@ -0,0 +1,47 @@
+
+3:00 to 4:00 ---> WebTable
+4:00 to 4.20 ---> Breakout+Break
+4:20 to 5:00 ---> AdvancedUserInteractions
+5:00 to 5:20 ---> Breakout+Break
+5:20 to 6:00 ---> TestNG basics
+6:00 to 6:15 --->Recap
+
+webtable:
+   multiple rows and columns---->identification-->//table in DOM
+   types:
+   1)static--->no of rows and columns remins same(values inside may change)
+   2(dynamic--->no of rowsand columns will change based on the inputs..
+
+   stucture of a webtable:
+
+   <table>
+   
+     <thead>---->table header
+       <tr>---->table row
+          <th>--->table header info
+          
+          </th>
+       
+       </tr>
+   
+     </thead>
+     <tbody>
+        <tr>
+         <td>---->table data
+         
+         </td>
+        </tr>
+
+        <tr>
+          <td>
+          </td>
+        
+        </tr<
+     
+     </tbody>
+        
+   
+   
+   
+   
+   </table>
